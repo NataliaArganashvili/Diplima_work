27c829c3a7da2646bca9f7c2e20138d76db0c040
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-11-09 01:49:57 +0400

    Update test\test_api.py

--- test\test_api.py
+++ test\test_api.py
@@ -1,5 +1,10 @@
 import requests
 
+@allure.title("Проверка сайта aviasales.ru поиска авиабилетов")
+@allure.description("Проверка сайта поиска авиабилетов")
+@allure.feature("GET") 
+@allure.severity("blocker")
+
 base_url = "https://min-prices.aviasales.ru/price_matrix?"
 
 my_headers = {
@@ -11,9 +16,11 @@ def test_get_oneway_positive() -> str:
     city1 = "MOW"
     city2 = "STW"
     date = "2024-12-15"
-    tickets_list = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
+    with allure.step("Получение списка билетов в одну сторону"):
+        tickets_list = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
     lst = tickets_list.json()
-    assert len(lst) > 0
+    with allure.step("Проверка"):
+        assert len(lst) > 0
 
 def test_get_twoways_positive() -> str:
     """Поиск билета в обе стороны - позитивная проверка"""
@@ -21,9 +28,11 @@ def test_get_twoways_positive() -> str:
     city2 = "STW"
     date1 = "2024-12-15"
     date2 = "2024-12-25"
-    tickets_list = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date1}&return_start={date2}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
+    with allure.step("Получение списка билетов в обе стороны"):
+        tickets_list = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date1}&return_start={date2}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
     lst = tickets_list.json()
-    assert len(lst) > 0
+    with allure.step("Проверка"):
+        assert len(lst) > 0
 
 def test_get_date_31_positive() -> str:
     """Поиск билета с числом 31 месяца с 31 днями - позитивная проверка"""
@@ -31,22 +40,28 @@ def test_get_date_31_positive() -> str:
     city2 = "STW"
     date1 = "2024-12-15"
     date2 = "2024-12-31"
-    tickets_list = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date1}&return_start={date2}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
+    with allure.step("Получение списка билетов на дату 31 месяца с 31 днями"):
+        tickets_list = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date1}&return_start={date2}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
     lst = tickets_list.json()
-    assert len(lst) > 0
+    with allure.step("Проверка"):
+        assert len(lst) > 0
 
 def test_get_date_31_negative() -> str:
     """Поиск билета с числом 31 месяца с 30 днями - негативная проверка"""
     city1 = "MOW"
     city2 = "NOZ"
     date = "2024-11-31"
-    resp = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
-    assert resp.status_code == 400
+    with allure.step("Проверка даты 31 для месяца с 30 днями"):
+        resp = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
+    with allure.step("Проверка"):
+        assert resp.status_code == 400
 
 def test_get_invalid_city_code() -> str:
     """Поиск билета в город с невалидным кодом - негативная проверка"""
     city1 = "MOW"
     city2 = "QWE"
     date = "2024-11-30"
-    resp = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
-    assert resp.status_code == 400
+    with allure.step("Проверка невалидного кода города"):
+        resp = requests.get(base_url + f'origin_iata={city1}&destination_iata={city2}&depart_start={date}&depart_range=6&return_range=6&affiliate=false&market=ru', headers=my_headers) 
+    with allure.step("Проверка"):
+        assert resp.status_code == 400
