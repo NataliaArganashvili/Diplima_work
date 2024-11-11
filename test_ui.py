57b86f591baf29694150d330b96ff8c6430b2a4b
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-11-11 14:55:52 +0400

    Update test_ui.py

--- test_ui.py
+++ test_ui.py
@@ -10,29 +10,35 @@ from pages_aviasales.main_page import MainPage
 @allure.title("Проверка открытия страницы aviasales.ru и автоматическое заполнение города")
 @allure.severity("medium")
 def test_open_page():
-    current_city = "Москва"
-    main_page = MainPage(driver)
-    assert driver.find_element(By.ID, "avia_form_origin-input").get_attribute("value") == current_city
+    current_city = "Москва" #Подставить город, где вы находитесь (с аэропортом)
+    with allure.step("Открыть страницу"):
+        main_page = MainPage(driver)
+    with allure.step("Проверка автоматического заполнения города"):
+        assert driver.find_element(By.ID, "avia_form_origin-input").get_attribute("value") == current_city
     driver.quit()
 
 @allure.title("Проверка того, что открывается страница https://ostrovok.ru/ при поиске билетов")
 @allure.severity("medium")
 def test_hotels():
     main_page = MainPage(driver)
-    main_page.fill_origin("Москва")
-    main_page.fill_destination("Новокузнецк")
+    with allure.step("Заполнить город отправления"):
+        main_page.fill_origin("Москва")
+    with allure.step("Заполнить город назначения"):
+        main_page.fill_destination("Новокузнецк")
     main_page.open_calender()
     main_page.choose_date("12.12.2024")
     main_page.click_search()
-    assert "https://ostrovok.ru/" in driver.current_url
+    with allure.step("Проверка того, что открывается страница https://ostrovok.ru/ при поиске билетов"):
+        assert "https://ostrovok.ru/" in driver.current_url
 
 @allure.title("Проверка загаловка страницы")
 @allure.severity("medium")
 def test_text():
-    driver.get("https://www.aviasales.ru/")
+    main_page = MainPage(driver)
     expected_text = "Тут покупают дешёвые авиабилеты"
     header = driver.find_element(By.CSS_SELECTOR, "h1.header__title")
-    assert header.text == expected_text
+    with allure.step("Проверка того, что в заголовке указан нужный текст"):
+        assert header.text == expected_text
 
 @allure.title("Негативный тест на проверку заполнения без города")
 @allure.severity("critical")
@@ -40,7 +46,8 @@ def test_negative_destination():
     main_page = MainPage(driver)
     main_page.click_search()
     element = driver.find_element(By.XPATH, '//div[text()="Укажите город прибытия"]')
-    assert element.is_displayed()
+    with allure.step("Проверка того, что незаполненное поле Куда подчвечивается"):
+        assert element.is_displayed()
 
 @allure.title("Негативный тест на проверку заполнения без даты")
 @allure.severity("critical")
@@ -48,4 +55,5 @@ def test_negative_date():
     main_page = MainPage(driver)
     main_page.click_search()
     element = driver.find_element(By.XPATH, '//div[text()="Укажите дату"]')
-    assert element.is_displayed()
+    with allure.step("Проверка того, что незаполненное поле Дата подчвечивается"):
+        assert element.is_displayed()
