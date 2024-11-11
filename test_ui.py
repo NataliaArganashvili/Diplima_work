c42ab58e2f0ccbba7de4cfa360304efca4450895
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-11-11 14:20:13 +0400

    Update and rename test\test_ui.py to test_ui.py

--- test_ui.py
+++ test_ui.py
@@ -7,15 +7,9 @@ driver.implicitly_wait(10)
 
 from pages_aviasales.main_page import MainPage
 
-@allure.title("Проверка сайта aviasales.ru")
-@allure.description("Проверка сайта поиска авиабилетов")
-@allure.feature("GET") 
-@allure.severity("blocker")
-
 def test_open_page():
     current_city = "Москва"
-    driver.implicitly_wait(10)
-    driver.get("https://www.aviasales.ru/")
+    main_page = MainPage(driver)
     assert driver.find_element(By.ID, "avia_form_origin-input").get_attribute("value") == current_city
     driver.quit()
 
@@ -29,21 +23,18 @@ def test_hotels():
     assert "https://ostrovok.ru/" in driver.current_url
 
 def test_text():
-    driver.implicitly_wait(10)
     driver.get("https://www.aviasales.ru/")
     expected_text = "Тут покупают дешёвые авиабилеты"
     header = driver.find_element(By.CSS_SELECTOR, "h1.header__title")
     assert header.text == expected_text
 
 def test_negative_destination():
-    driver.implicitly_wait(10)
     main_page = MainPage(driver)
     main_page.click_search()
     element = driver.find_element(By.XPATH, '//div[text()="Укажите город прибытия"]')
     assert element.is_displyed()
 
 def test_negative_date():
-    driver.implicitly_wait(10)
     main_page = MainPage(driver)
     main_page.click_search()
     element = driver.find_element(By.XPATH, '//div[text()="Укажите дату"]')
